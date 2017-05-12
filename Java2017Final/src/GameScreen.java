9b623c09a3b312930eaecc0eedd89a2885c50b82
hwang635 <hwang635@PR31.hhs.win.fuhsd.org>
2017-05-12 09:16:02 -0700

    more Changes!!!!

--- Java2017Final/src/GameScreen.java
+++ Java2017Final/src/GameScreen.java
@@ -1,6 +1,8 @@
 import java.awt.Color.*;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,23 +11,38 @@ import javax.swing.JPanel;
 //graphics of the game screen, background 
 //has character, slingshot, target, + the menu w/ the helper blocks
 
-public class GameScreen extends JPanel {
+public class GameScreen extends JPanel implements KeyListener {
 
 	private Slingshot slingshot;
-	private Character character;
+	//private Character character;
 	private Target target;
 	
 	private GameScreen () {
 		slingshot = new Slingshot();
-		character = new Character();
+		//character = new Character();
 		target = new Target();
 		
-		Color SKYBLUE = new Color(30, 155, 255);
+		Color SKYBLUE = new Color(175, 238, 238);
 		setBackground(SKYBLUE);
 	}
 
 	public void paintComponent (Graphics g) {
 		super.paintComponent(g);
+		
+		int width = getWidth();
+		int height = getHeight();
+		
+		//platform for character
+		Color PALEGREEN = new Color(152, 251, 152);
+		g.setColor(PALEGREEN);
+		g.fillRect(0,400, 150, 200);
+		
+		
+		
+		//platform for target
+		
+		//screen
+		
 	}
 
 
@@ -38,5 +55,23 @@ public class GameScreen extends JPanel {
 		w.add(panel);
 		w.setResizable(false);
 		w.setVisible(true);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
