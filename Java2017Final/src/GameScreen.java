ef73513d37049b8ac4cb6d27089b5f26be336afd
phil <phil@LenovoLark.Hemiwaly.com>
2017-05-12 20:42:45 -0700

    error rn will fix

--- Java2017Final/src/GameScreen.java
+++ Java2017Final/src/GameScreen.java
@@ -3,6 +3,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,38 +13,50 @@ import javax.swing.JPanel;
 //graphics of the game screen, background 
 //has character, slingshot, target, + the menu w/ the helper blocks
 
-public class GameScreen extends JPanel implements KeyListener {
+public class GameScreen extends JPanel implements MouseListener {
 
 	private Slingshot slingshot;
 	//private Character character;
 	private Target target;
-	
+
+	private boolean helperObj, drawHelperObj;
+	private int xClick, yClick;
+
 	private GameScreen () {
 		slingshot = new Slingshot();
 		//character = new Character();
-		target = new Target();
-		
+		target = new Target();		
+
+		helperObj = false;
+
 		Color SKYBLUE = new Color(175, 238, 238);
 		setBackground(SKYBLUE);
 	}
 
 	public void paintComponent (Graphics g) {
 		super.paintComponent(g);
-		
+
 		int width = getWidth();
 		int height = getHeight();
-		
+
 		//platform for character
 		Color PALEGREEN = new Color(160, 255, 100);
 		g.setColor(PALEGREEN);
 		g.fillRect(0, 400, 150, 200);		
-		
+
 		//platform for target
 		g.fillRect(500, 300, 150, 300);
-		//screen
+
+		//screen w/ all the things
 		Color LIGHTGRAY = new Color(211, 211, 211);
 		g.setColor(LIGHTGRAY);
 		g.fillRect(650, 0,  150, height);
+		g.setColor(Color.WHITE);
+		g.fillRect(700, 150, 50, 20);
+
+		if(drawHelperObj == true) {
+			g.fillRect(xClick, yClick, 50, 20);
+		}
 	}
 
 
@@ -55,23 +69,52 @@ public class GameScreen extends JPanel implements KeyListener {
 		w.add(panel);
 		w.setResizable(false);
 		w.setVisible(true);
+
+		panel.addMouseListener(panel);
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void mouseClicked(MouseEvent arg0) {
+
 	}
 
 	@Override
-	public void keyReleased(KeyEvent e) {
+	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {
+	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		xClick = e.getX();
+		yClick = e.getY();
+		int button = e.getButton();
+
+		if(button == MouseEvent.BUTTON1) {
+			if(helperObj == false) {
+				if(xClick>= 700 && xClick<=750 && yClick>=150 && yClick <=170)
+					helperObj = true;
+			}
+			//if true, which means have already clicked on a block
+			else {
+				drawHelperObj = true;
+				helperObj = false;
+			}
+		}
+		
+		repaint();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
