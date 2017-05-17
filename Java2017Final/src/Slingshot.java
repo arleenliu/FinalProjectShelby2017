2d165367362868eb85fa5e2509a62fb3487ad855
arleenliu <arleenliu@10.16.83.89>
2017-05-17 14:28:42 -0700

    faulty math

--- Java2017Final/src/Slingshot.java
+++ Java2017Final/src/Slingshot.java
@@ -1,61 +1,106 @@
 import java.awt.Image;
 
 public class Slingshot {
-	
-	//FIELDS
+
+	// FIELDS
 	private Image slingshotImg;
 	private static final double GRAVITY = 9.81;
 	private int height, width;
 	private int length;
-	int velocity;
-	
-	//CONSTRUCTORS
+	// velocityI calculation will be figured out later
+	private int velocityI, velocityF;
+	private int xVelocity, yVelocityI, yVelocityF;
+	private int jumpNum;
+	private double timeTaken;
+	private int objectHeight;
+
+	// CONSTRUCTORS
+	// to be deleted later
 	public Slingshot() {
-		
+
 	}
 	
-	public Slingshot(Image slingshot, int h, int w) {
+	public int calculateCurrentXVelocity() {
+		return xVelocity;
+	}
+
+	public Slingshot(Image slingshot, int h, int w, int length) {
 		slingshotImg = slingshot;
 		height = h;
 		width = w;
-		length = 0;
-		velocity = 0;
-	}	
-	
-	public void setLength(int length) {
 		this.length = length;
+		setVelocity();
+		velocityI = 0;
+		velocityF = 0;
+		jumpNum = 0;
+		timeTaken = 0;
+		objectHeight = 0;
 	}
-	
-	public int getLength() {
-		return length;
+
+	private void setVelocity() {
+		if (jumpNum == 0) {
+			velocityI = (int) (length * GRAVITY);
+		} else {
+			velocityI = (int) (velocityF / 2.0);
+			velocityF = 0;
+		}
+		
+		jumpNum++;
+
 	}
-	
-	public int calculateDistance(int length, double angle) {
+
+	public void setTimeTaken(double beginTime, double endTime) {
+		timeTaken = endTime - beginTime;
+	}
+
+	public double getTimeTaken() {
+		return timeTaken;
+	}
+
+	public void setObjectHeight(int heightI, int heightF) {
+		objectHeight = heightF - heightI;
+	}
+
+	public int getObjectHeight() {
+		return objectHeight;
+	}
+
+	public int calculateDistance(double angle) {
 		
-		velocity = (int) (length * GRAVITY);
-		this.length = length;
-		int distance = (int) (Math.pow(velocity, 2) * Math.sin(2 * angle) / GRAVITY);
 		
-		distance = distance + (int) (height * Math.sin(90 - angle) / Math.sin(angle));
-		
+		xVelocity = (int) (velocityI * Math.cos(angle));
+
+		int distance = (int) (timeTaken * xVelocity);
+
+		setVelocity();
+		jumpNum++;
+
 		return distance;
-	} 
-	
-	public int getVelocity() {
-		velocity = (int) (length * GRAVITY);
-		return velocity;
 	}
-	
-/*	public int calculateMaxHeight(int length, double angle, TimeTracker time) {
-		
-		this.length = length;
-		int maxHeight = (int) (height + (velocity * time / 2.0) + (0.5 * GRAVITY * Math.pow(time, 2)));
-		
-		return maxHeight;
-	}*/
-	
-	//METHODS
-	
-	
-	
+
+	public int calculateFinalVelocity(double angle) {
+
+		yVelocityI = (int) (velocityI * Math.sin(angle));
+		yVelocityF = (int) (Math.sqrt(Math.pow(yVelocityI, 2) - 2 * GRAVITY * objectHeight));
+		velocityF = (int) (Math.sqrt(Math.pow(xVelocity, 2) + Math.pow(yVelocityF, 2)));
+
+		return velocityF;
+	}
+
+	public int getJumpNum() {
+		return jumpNum;
+	}
+
+	/*
+	 * public int calculateMaxHeight(int length, double angle, TimeTracker time)
+	 * {
+	 * 
+	 * this.length = length; int maxHeight = (int) (height + (velocity * time /
+	 * 2.0) + (0.5 * GRAVITY * Math.pow(time, 2)));
+	 * 
+	 * return maxHeight; }
+	 */
+
+	// METHODS
+
 }
