021a04d01bcb61cfe63df7a0a4d0c4d8200c2144
arleenliu <arleenliu@10.16.83.89>
2017-05-21 21:30:31 -0700

    helen is dumb

--- Java2017Final/src/Fling.java
+++ Java2017Final/src/Fling.java
@@ -2,7 +2,7 @@ import java.awt.Component;
 
 import javax.swing.JFrame;
 
-public class Fling {
+public class Fling extends JFrame{
 	
 	//title is 6, level is 7, game is 8, result is 9
 	public static int Screen;
@@ -10,19 +10,21 @@ public class Fling {
 	//levels one-five are 1-5
 	public static int level;
 	
-	public Fling() {
+	public Fling(String name) {
 		Screen = 6;
 	}
 
 	public static void main(String[] args)
 	{
-		JFrame w = new JFrame("Fling");
+		Fling w = new Fling("Fling");
 		w.setBounds(100, 100, 800, 600);
 		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GameScreen panel = new GameScreen(level);
 		TitleScreen titlePanel = new TitleScreen();
 		ResultScreen resultPanel = new ResultScreen();
 		LevelScreen levelPanel = new LevelScreen();
+		
+		System.out.println(Screen + "");
 		
 		if(Screen != 6) {
 			w.add(titlePanel);
