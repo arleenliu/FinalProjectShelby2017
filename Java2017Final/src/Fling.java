2c26f08072c5c8e455b564b3daee3f0b9abf9089
jannie <jannie@LAPTOP-4H04VI9T.pokemon>
2017-05-21 22:52:01 -0700

    fgdfgd

--- Java2017Final/src/Fling.java
+++ Java2017Final/src/Fling.java
@@ -1,44 +1,49 @@
-import java.awt.Component;
-
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 public class Fling extends JFrame{
 	
 	//title is 6, level is 7, game is 8, result is 9
 	public static int Screen;
+	private static TitleScreen titlePanel;
+	private static ResultScreen resultPanel;
+	private static LevelScreen levelPanel;
+	private static GameScreen panel;
+	private static Fling w;
 	
 	//levels one-five are 1-5
 	public static int level;
 	
 	public Fling(String name) {
 		Screen = 6;
+		
 	}
 
 	public static void main(String[] args)
 	{
-		Fling w = new Fling("Fling");
+		w = new Fling("Fling");
 		w.setBounds(100, 100, 800, 600);
 		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GameScreen panel = new GameScreen(level);
-		TitleScreen titlePanel = new TitleScreen();
-		ResultScreen resultPanel = new ResultScreen();
-		LevelScreen levelPanel = new LevelScreen();
 		
-		System.out.println(Screen + "");
+		panel = new GameScreen(level);
+		titlePanel = new TitleScreen();
+		resultPanel = new ResultScreen();
+		levelPanel = new LevelScreen();
+		
+		w.add(levelPanel);
+		w.add(panel);
+		w.add(resultPanel);
+		w.add(titlePanel);
 		
 		if(Screen == 6) {
-		
-			w.add(titlePanel);
-			System.out.println(Screen + "");
-			System.out.println(level + "");
-
+			w.getComponent(0);
 		}
 		else if(Screen == 7)
-			w.add(levelPanel);
+			w.getComponent(1);
 		else if(Screen == 8)
-			w.add(panel);
+			w.getComponent(2);
 		else if(Screen == 9)
-			w.add(resultPanel);
+			w.getComponent(3);
 		
 		w.setResizable(false);
 		w.setVisible(true);
@@ -48,6 +53,27 @@ public class Fling extends JFrame{
 		titlePanel.addMouseListener(titlePanel);
 		resultPanel.addMouseListener(resultPanel);
 		levelPanel.addMouseListener(levelPanel);
+		
+		w.run();
+	}
+	
+	public void run(){
+		
+		while (true){
+			if(Screen == 6) {
+				w.getComponent(0);
+			}
+			else if(Screen == 7)
+				w.getComponent(1);
+			else if(Screen == 8)
+				w.getComponent(2);
+			else if(Screen == 9)
+				w.getComponent(3);
+
+			System.out.println(Screen + "");
+			
+		}
+		
 	}
 	
 }
