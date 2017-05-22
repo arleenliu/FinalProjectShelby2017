8e5f53ea55a405809b1da554778f86675cb8fc35
phil <phil@LenovoLark.pokemon>
2017-05-21 20:02:13 -0700

    wrwerw

--- Java2017Final/src/Fling.java
+++ Java2017Final/src/Fling.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class Fling {
 	
 	//levels one - five are 1-5
-	//title is 6, level is 7, game is 8, result is 9
+	//title is 6, level is 7, game is 8, result is 
 	public static int Screen;
 	
 	public Fling() {
@@ -15,11 +15,11 @@ public class Fling {
 		JFrame w = new JFrame("Fling");
 		w.setBounds(100, 100, 800, 600);
 		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GameScreen panel = new GameScreen(4);
+		GameScreen panel = new GameScreen(1);
 		TitleScreen titlePanel = new TitleScreen();
 		ResultScreen resultPanel = new ResultScreen();
 		LevelScreen levelPanel = new LevelScreen();
-		w.add(levelPanel);
+		w.add(panel);
 		w.setResizable(false);
 		w.setVisible(true);
 
